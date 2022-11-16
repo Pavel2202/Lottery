8e16ffe6fac366878c8701f46be8e8380619df82
Pavel2202 <paveltimenov3@gmail.com>
2022-11-16 15:00:25 +0200

    Implement logic for entering the lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.17;
+
+error Lottery__NotEnoughETHEntered();
+
+contract Lottery {
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    event LotteryEnter(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterLottery() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Lottery__NotEnoughETHEntered();
+        }
+
+        s_players.push(payable(msg.sender));
+        emit LotteryEnter(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
