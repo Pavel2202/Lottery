5d1c15dd0a7da936276cc20779aceca4f162e1c4
Pavel2202 <paveltimenov3@gmail.com>
2022-11-20 13:00:10 +0200

    Add gitignore and depploy scripts

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,10 @@
+const { developmentChains } = require("../helper-hardhat-config");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+  const { deploy, log } = deployments;
+  const deployer = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  if (developmentChains.includes(network.name)) {
+  }
+};
