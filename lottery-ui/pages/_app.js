43c0a40b3edb2308c8f23a744ac00733b82b8a9b
Pavel2202 <paveltimenov3@gmail.com>
2023-03-13 17:57:47 +0200

    Add Header with connect button

--- lottery-ui/pages/_app.js
+++ lottery-ui/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import { MoralisProvider } from "react-moralis";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider initializeOnMount={false}>
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
