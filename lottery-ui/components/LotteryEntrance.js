40e06a277ac75457bf3cdd9da546a166bf19f8f8
Pavel2202 <paveltimenov3@gmail.com>
2023-03-14 11:50:07 +0200

    Add script for generating contract address and abi

---
+++ lottery-ui/components/LotteryEntrance.js
@@ -0,0 +1,14 @@
+import { useWeb3Contract } from "react-moralis";
+
+const LotteryEntrance = () => {
+  // const {runContractFunction: enterLottery} = useWeb3Contract({
+  //     abi: ,
+  //     contractAddress: ,
+  //     functionName: ,
+  //     params: {},
+  // })
+
+  return <div></div>;
+};
+
+export default LotteryEntrance;
--- lottery-ui/pages/index.js
+++ lottery-ui/pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
 import Header from "../components/Header";
+import LotteryEntrance from "../components/LotteryEntrance";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <LotteryEntrance />
     </div>
   );
 }
